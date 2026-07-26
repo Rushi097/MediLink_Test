727b9c47bdd5cb231b336f0d56744d11c98e19fb
Shraddha-Official <sshraddha.2025patilb@gmail.com>
2026-07-26 23:56:53 +0530

    add bubble messege

--- Web/medilink-react/src/components/chatbot/Medibot.jsx
+++ Web/medilink-react/src/components/chatbot/Medibot.jsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from "react";
 import "./Medibot.css";
 import { RiRobot2Line } from "react-icons/ri";
 
 function MediBot() {
+
+    const [showWelcome, setShowWelcome] = useState(false);
+
+    useEffect(() => {
+        const showTimer = setTimeout(() => {
+            setShowWelcome(true);
+        }, 3000);
+
+        const hideTimer = setTimeout(() => {
+            setShowWelcome(false);
+        }, 8000);
+
+        return () => {
+            clearTimeout(showTimer);
+            clearTimeout(hideTimer);
+        };
+    }, []);
+
     return (
-        <button className="medibot-btn">
-            <RiRobot2Line />
-        </button>
+        <>
+            {showWelcome && (
+                <div className="welcome-bubble">
+                    <p>👋 Need any help?</p>
+                    <span>Ask MediBot anytime.</span>
+                </div>
+            )}
+            <button className="medibot-btn">
+                <RiRobot2Line />
+            </button>
+        </>
     );
 }
 
